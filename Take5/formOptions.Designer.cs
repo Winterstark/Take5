ddef39f8ee949c117a82cce59c1c1a98423ab2f7
MrCull <culllcfc@yahoo.co.uk>
2016-01-30 15:16:32 +0000

    Options form to use the same icon as the tray icon
    Options form to use the same icon as the tray icon.
    It looks a lot better than the standard .NET icon.

--- Take5/formOptions.Designer.cs
+++ Take5/formOptions.Designer.cs
@@ -276,6 +276,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "formOptions";
             this.Text = "Take5 Options";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.formOptions_FormClosed);
