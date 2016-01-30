3e1f27ff873f734b79ecd2a97bef9ec00b16ccd7
MrCull <culllcfc@yahoo.co.uk>
2016-01-30 15:21:09 +0000

    Options form to use the same icon as the tray icon
    Options form to use the same icon as the tray icon.
    It looks a lot better than the standard .NET icon

--- Take5/formOptions.Designer.cs
+++ Take5/formOptions.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formOptions));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
